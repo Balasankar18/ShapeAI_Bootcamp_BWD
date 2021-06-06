df29e12d15ad9aea7c8b78a93f986e810db45b51
M. Balasankar <85424210+Balasankar18@users.noreply.github.com>
2021-06-06 11:28:25 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcamp taken by shaurya sinha we covered everthing
+        from scratch including Javascript React.js, HTML.
+        This project is done by M.Balasankar.
+        This project is done by help of 7 days bootcamp and 
+        this project is submit for ShapeAI in Github. 
+      </p>
+    </div>
+  );
+}
+
+export default Note;
